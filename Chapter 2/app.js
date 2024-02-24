9b2e8a4f6676a69e3832ca8aabefc8818054f656
Muhammadbilal1257727 <bilalkhan1258899@gmail.com>
2024-02-24 07:18:48 -0800

    JS Practise Variable & Alert

---
+++ Chapter 2/app.js
@@ -0,0 +1,14 @@
+var userName = "Ahmed";
+var userAge = "15"
+var message ="HelloWorld";
+var email = "xyz@gmail.com";
+var book = "“Asmarter way to learn JavaScript";
+var start = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+
+alert(start);
+alert("User Name" +" "+ userName);
+alert("User Age" +" "+ userAge);
+alert("User Eamil" +" "+ email);
+alert(message);
+alert(book);
+alert(start);
