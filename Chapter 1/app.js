38407e088fdc34ab643802a314a1d4cc30fe6b26
Muhmmad Bilal Khan <bilalkhan1258899@gmail.com>
2024-02-23 22:36:56 -0800

    JS Practise

---
+++ Chapter 1/app.js
@@ -0,0 +1,6 @@
+alert("Welcome To JS Land");
+alert("Welcome To Land...... Js Happy Coding :)");
+alert("Prevent This Creating From Additional Dialog");
+alert("Hello Please Enter Your Detail");
+prompt("Enter Password");
+alert("Error! Please Enter a Valid Password");
