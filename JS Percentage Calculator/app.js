61db732832ae5ce06b71e69ba08813620da81fc8
Muhammadbilal12577 <bilalkhan1258899@gmail.com>
2024-02-27 08:09:13 -0800

    JS Percentage Calculator

---
+++ JS Percentage Calculator/app.js
@@ -0,0 +1,19 @@
+var English = +prompt("English Mark")
+var Math = +prompt("Math Mark")
+var Urdu = +prompt("Urdu Mark")
+var Islamiat = +prompt("Islamiat Mark")
+var Physic = +prompt("Physic")
+
+var total = 500
+
+console.log("Total Marks" , total)
+
+var obtained= ( English + Math + Urdu + Islamiat + Physic)
+console.log("Obtained " , obtained)
+
+var percentage= obtained / total * 100
+console.log (percentage)
+
+ 
+
+
