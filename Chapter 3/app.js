a7354ed2411cb8ff0c4959ace59ac9af10f6fd7b
Muhammadbilal12577 <bilalkhan1258899@gmail.com>
2024-02-27 08:05:48 -0800

    JS Practise VARIABLES FOR NUMBERS

---
+++ Chapter 3/app.js
@@ -0,0 +1,10 @@
+var coustomerName = prompt("Your Name")
+var birthYear ="Your Birth Year Is 2005"
+var age = "Your Age is 18"
+var visit = "You Visit This Site 4 time"
+var product = prompt("Which Product You Want" )
+alert(coustomerName)
+alert(birthYear)
+alert(age)
+alert(visit)
+alert(product)
