68c29c561c313900c7cc70f0c43f9030f59e0a48
Muhammadbilal12577 <bilalkhan1258899@gmail.com>
2024-02-28 09:52:35 -0800

    Assignment

---
+++ JS If else assigiment/Question 11/app.js
@@ -0,0 +1,8 @@
+var firstName = "Ali";
+
+
+if (firstName === "Fahad") {
+    document.write("Hello Fahad!");
+   }else{
+    console.log("You Are Not Fahad")
+   }
---
+++ JS If else assigiment/Question 12/app.js
@@ -0,0 +1,6 @@
+var time=prompt("Enter Time")
+if (time < 14) {
+    console.log ("Good day");
+} else {
+    console.log ( "Good evening");
+}
