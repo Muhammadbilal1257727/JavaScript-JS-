8dac764fba81acdc509e22cc7c19cde1a3ea9e19
Muhammadbilal1257727 <bilalkhan1258899@gmail.com>
2024-03-01 21:29:57 -0800

    Functions Practise

---
+++ JS Functions/app.js
@@ -0,0 +1,71 @@
+// FUNCTION PRACTISE 
+
+
+
+
+
+
+// function username (){
+//     console.log("Muhammad bilal khan")
+// }
+// username()
+
+
+
+
+
+
+
+
+// function percentage(mark1 , mark2 , mark3 , mark4 , mark5){
+//     var total = 500
+//     var obtained = mark1 + mark2 + mark3 + mark4 + mark5 
+//     console.log(total % obtained   )
+// }
+// percentage(50,30,50,40,45,)
+
+
+
+
+// var head = document.querySelector('.head');
+// console.log(head.innerHTML);
+
+
+
+// // For changing something is html using we use this
+// head.innerHTML = "change throug js"
+// console.log(head.innerHTML)
+
+
+
+
+
+// var paragraph = document.querySelector('.para')
+// console.log(paragraph.innerHTML)
+
+// // For changing something is html using we use this
+// paragraph.innerHTML = "Hi this is bilalKhan Front ende developer"
+// console.log(paragraph.innerHTML)
+
+
+
+// function percentagecal(mark1 , mark2 , mark3 , mark4 , mark5){
+//     var totalmarks = 500
+//     var obtainedmarks = mark1 + mark2 + mark3 + mark4 + mark5 
+//     return obtainedmarks/totalmarks*100
+// }
+// var xyzpercentage = percentagecal(20,40,60,90,80)
+// console.log(xyzpercentage);
+
+
+
+function sum(sum1 , sum2 , sum3 , sum4 , sum5){
+    console.log(sum1 + sum2 + sum3 + sum4 + sum5)
+}
+sum(20,30,50,740,80);
+
+
+
+
+
+
