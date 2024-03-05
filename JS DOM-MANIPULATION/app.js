8a9e246d5fa8d69029a51d5bc04ebc38fa18d9f2
Muhammadbilal1257727 <bilalkhan1258899@gmail.com>
2024-03-04 22:12:00 -0800

    DOM & MANUPILTION

---
+++ JS DOM-MANIPULATION/app.js
@@ -0,0 +1,33 @@
+var subject =document.querySelector(".sub1 ")
+var subjecT =document.querySelector(".sub2 ")
+
+
+function mark(){
+    console.log(subject.value)
+    input.value=''
+}
+
+
+
+
+// function mark(){
+//     console.log(subject.value)
+//     input.value=''
+// }
+
+
+// var subject =document.querySelector(".sub3 ")
+
+// function marks(){
+//     console.log(subject.value)
+//     input.value=''
+// }
+
+
+// var subject =document.querySelector(".sub4 ")
+
+// function marks(){
+//     console.log(subject.value)
+//     input.value=''
+// }
+
