f22798e64d85f51203baa9dd261c9672a27e536f
Muhammadbilal12577 <bilalkhan1258899@gmail.com>
2024-03-09 12:21:30 +0500

    JavaScprit Practise

---
+++ JS Array/app.js
@@ -0,0 +1,50 @@
+// let = var = const
+// Array Data Type is Object
+// Array is []
+
+
+// let cites = ['Bilal' , 'Ali Hassan' , 'Bilal Bahi' , 'Huzafia' ,['Karachi' , 'Lohare' , 'Pindi']]
+// console.log(cites[2] );
+
+
+                                          // Array Function
+
+
+// Push Function ya akir ma name add karta ha 
+// let furits=["malta", "anda wala" , "Lassi"]
+// furits.push('Punjabi')
+// console.log(furits);
+
+
+
+// Pop Function ya akir sa name remove karta ha
+// let furits=["malta", "anda wala" , "Lassi"]
+// furits.pop()
+// console.log(furits);
+
+
+// unshift function ya shuro ma name add karta ha 
+// let furits=["malta", "anda wala" , "Lassi"]
+// furits.unshift("Huzaifa ")
+// console.log(furits);
+
+
+// Shift function ya shuro ma sa name remove karta ha
+// let furits=["malta", "anda wala" , "Lassi"]
+// furits.shift()
+// console.log(furits);
+
+
+
+
+
+// Splice Function slecect,remove&add
+// let furits=["malta", "anda wala" , "Lassi" , "Anda wala" , "Kela"]
+// furits.splice(3 , 1 , "Pinaple" , 'Sugar Cane ' , "Gta5")
+// console.log(furits);
+
+
+
+// Slice function Make Copy
+
+
---
+++ js parc/app.js
@@ -0,0 +1,25 @@
+
+// this is for paragraph 
+// var head = document.querySelector(".head")
+// head.innerHTML="Change Thruogh JS"
+// console.log(head.innerHTML)
+
+
+// function button(){
+//     var head = document.querySelector(".head")
+//  head.innerHTML="Change Thruogh JS"
+//  console.log(head.innerHTML)
+
+// }
+
+
+// for input value
+// var input =document.querySelector("#input")
+// // console.log(input.value)
+
+// function getvalue(){
+//     console.log(input.value)
+//     input.value=""
+// }
+
+
