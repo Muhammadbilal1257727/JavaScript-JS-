bae04b23350c4b96a312f1e10f8f33ffd37a1d4f
Muhammadbilal1257727 <bilalkhan1258899@gmail.com>
2024-03-14 00:47:09 -0700

    JS

---
+++ JS Array Function Practise/app.js
@@ -0,0 +1,30 @@
+// There are 12 type of Array Function
+
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+
+
+
+// // Push
+// let names = ["Bilal","Hassan","Huzaifa"]
+// names.push("Mowyia")
+// console.log(names); 
+
+
+
+// Pop
+let names = ["Bilal","Hassan","Huzafia"]
+names.pop("Mowyia")
+console.log(names)
