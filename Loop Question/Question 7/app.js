cdeaafeca33511e1beca81a5d92fa83d2b899baa
Muhammadbilal1257727 <bilalkhan1258899@gmail.com>
2024-03-23 10:21:00 -0700

    JS

--- Loop Question/Question 7/app.js
+++ Loop Question/Question 7/app.js
@@ -1,8 +1,24 @@
-var N = parseInt(prompt("Enter the number of items:"));
-var items = [];
-for (var i = 0; i < N; i++) {
-    var item = prompt("Enter item " + (i + 1) + ":");
-    items.push(item);
+document.write("<h3>a. Counting: </h3>");
+for (var i = 1; i <= 15; i++) {
+  document.write(i + ", ");
 }
-console.log("Items entered by the user:");
-console.log(items);
+
+document.write("<h3>b. Reverse counting: </h3>");
+for (var i = 10; i >= 1; i--) {
+  document.write(i + ", ");
+}
+
+document.write("<h3>c. Even: </h3>");
+for (var i = 0; i <= 20; i += 2) {
+  document.write(i + ", ");
+}
+
+document.write("<h3>d. Odd: </h3>");
+for (var i = 1; i <= 19; i += 2) {
+  document.write(i + ", ");
+}
+
+document.write("<h3>e. Series: </h3>");
+for (var i = 2; i <= 20; i += 2) {
+  document.write(i + "k, ");
+}
